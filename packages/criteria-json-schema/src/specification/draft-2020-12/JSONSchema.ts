c28b86353ab98f3482ad35d44e11d02791df5139
James Moschou <james.moschou@gmail.com>
2023-01-04 20:25:59 +1100

    Remove support for extensions to JSON Schema for now

--- packages/criteria-json-schema/src/specification/draft-2020-12/JSONSchema.ts
+++ packages/criteria-json-schema/src/specification/draft-2020-12/JSONSchema.ts
@@ -33,9 +33,4 @@ export type JSONSchema =
       JSONSchemaUnevaluatedApplicatorVocabulary &
       JSONSchemaFormatAnnotationVocabulary &
       JSONSchemaContentVocabulary &
-      JSONSchemaMetaDataVocabulary & {
-        /**
-         * @see https://datatracker.ietf.org/doc/html/draft-bhutton-json-schema-01#section-6.5
-         */
-        [key: string]: any
-      })
+      JSONSchemaMetaDataVocabulary)

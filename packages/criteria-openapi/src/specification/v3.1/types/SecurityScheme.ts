c846ecc64a7cef7fb426df0af71f4248aa65c7a2
James Moschou <james.moschou@gmail.com>
2023-02-07 21:47:49 +1100

    Fix Index signature for type '`x-${string}`' is missing in type

--- packages/criteria-openapi/src/specification/v3.1/types/SecurityScheme.ts
+++ packages/criteria-openapi/src/specification/v3.1/types/SecurityScheme.ts
@@ -1,19 +1,18 @@
 import { OAuthFlows } from './OAuthFlows'
 
-export type SecurityScheme = (
+export type SecurityScheme =
   | APIKeySecurityScheme
   | HTTPSecurityScheme
   | OAuth2SecurityScheme
   | OpenIDConnectSecurityScheme
-) & {
-  [key: `x-${string}`]: any
-}
 
 export interface APIKeySecurityScheme {
   type: 'apiKey'
   description?: string
   name: string
   in: 'query' | 'header' | 'cookie'
+
+  [key: `x-${string}`]: any
 }
 
 export interface HTTPSecurityScheme {
@@ -21,16 +20,22 @@ export interface HTTPSecurityScheme {
   description?: string
   scheme: string
   bearerFormat?: string
+
+  [key: `x-${string}`]: any
 }
 
 export interface OAuth2SecurityScheme {
   type: 'oauth2'
   description?: string
   flows: OAuthFlows
+
+  [key: `x-${string}`]: any
 }
 
 export interface OpenIDConnectSecurityScheme {
   type: 'openIdConnect'
   description?: string
   openIdConnectUrl: string
+
+  [key: `x-${string}`]: any
 }

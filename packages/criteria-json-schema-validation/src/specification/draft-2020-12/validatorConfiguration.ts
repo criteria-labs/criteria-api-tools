e648c2211eb49077762561ce10b928b2f0fa4d14
James Moschou <james.moschou@gmail.com>
2023-11-17 12:54:18 +1100

    Delete unused code

--- packages/criteria-json-schema-validation/src/specification/draft-2020-12/validatorConfiguration.ts
+++ packages/criteria-json-schema-validation/src/specification/draft-2020-12/validatorConfiguration.ts
@@ -1,13 +1,6 @@
 import { dereferenceJSONSchemaDraft2020_12 } from '@criteria/json-schema'
 import { ValidatorConfiguration } from '../../validation/jsonValidator'
-import { applicatorValidators } from './vocabularies/applicator'
-import { coreValidators } from './vocabularies/core'
-import { unevaluatedValidators } from './vocabularies/unevaluated'
-import { validationValidators } from './vocabularies/validation'
 
-// order is important here due to keyword interdependence
-// applicator before validation, so maxContains and minContains can access contains annotation results
-// add unevaluated keywords last
 const configuration: ValidatorConfiguration = {
   defaultMetaSchemaURI: 'https://json-schema.org/draft/2020-12/schema',
   dereferenceJSONSchema: dereferenceJSONSchemaDraft2020_12
